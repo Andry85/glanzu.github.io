f5609b190b8795fb2eb494e67ead22ca00df407a
andryShapoval <andryShapoval@gmail.com>
2017-02-04 13:24:22 +0200

    add new version dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,6 +21,41 @@ $(function() {
 						/* END CURRENCIES DROP-DOWN LIST  */
 	/********************************************************/
 
+
+	/*********************************************************/
+						/* START CURRENCIES DROP-DOWN LIST  */
+	/********************************************************/
+        
+        $('.header').append('<div class="hidMenu">');
+        var hidM = $(".hidMenu");
+
+        $(".js-roll").click(function() {
+          $(this).toggleClass("activ");
+          hidM.slideToggle("slow"); 
+		});
+        
+        
+        function onResize() {
+            var wiw = window.innerWidth;
+            if(wiw > 1358) {
+            	$(".js-link").prependTo('.nav__item--hasSub');
+            	$(".languages,.currencies").appendTo('.header__col3');
+            	hidM.hide();  
+            	$(".js-roll").removeClass("activ");
+            }
+            else {
+               $(".js-link,.languages,.currencies").appendTo(hidM);
+
+            }
+        }
+        onResize();
+        $(window).resize(onResize);
+
+        
+
+    /*********************************************************/
+						/* END CURRENCIES DROP-DOWN LIST  */
+	/********************************************************/    
 	
 
 
