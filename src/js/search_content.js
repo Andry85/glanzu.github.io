e744ba794f55678cdd1be0f803267b810f7f00da
andryShapoval <andryShapoval@gmail.com>
2017-02-05 19:38:22 +0200

    05.02.2017 final

---
+++ src/js/search_content.js
@@ -0,0 +1,14 @@
+var tipuedrop = {"pages": [
+     {	"title": "Блабландия", 
+     	"url": "https://www.google.com.ua/"
+     },
+     {	"title": "Малые Блаблавичи", 
+     	"url": "#"
+     },
+     {	"title": "Москва", 
+     	"url": "https://www.yandex.ru/"
+     },
+     {	"title": "Киев", 
+     	"url": "https://www.yandex.ua/"
+     }
+]};
